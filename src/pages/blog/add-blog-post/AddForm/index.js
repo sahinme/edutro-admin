47edfffed1377608efa106b0e246304fc6292849
sahinme <iamfreeadmiral@gmail.com>
2020-02-11 14:34:08 +0300

    .

--- src/pages/blog/add-blog-post/AddForm/index.js
+++ src/pages/blog/add-blog-post/AddForm/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Editor } from 'react-draft-wysiwyg'
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import FroalaEditorComponent from 'react-froala-wysiwyg';
 import { Form, Input, Button, Radio, Select, Upload, Icon } from 'antd'
+
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+
 import styles from '../style.module.scss'
+
 
 const FormItem = Form.Item
 const RadioGroup = Radio.Group
@@ -11,6 +16,9 @@ const { Dragger } = Upload
 @Form.create()
 class AddForm extends React.Component {
   render() {
+    /* const config = {
+      imageEditButtons: ['imageDisplay', 'imageAlign', 'imageInfo', 'imageRemove']
+    } */
     const { form } = this.props
 
     return (
@@ -51,7 +59,19 @@ class AddForm extends React.Component {
           <FormItem label="İçerik">
             {form.getFieldDecorator('content')(
               <div className={styles.editor}>
-                <Editor />
+                <FroalaEditorComponent
+                  config={{
+                    placeholderText: 'Edit Your Content Here!',
+                    charCounterCount: false,
+                    imageUpload: true,
+                    imageDefaultDisplay: 'inline-block',
+                    // Set max image size to 5MB.
+                    imageMaxSize: 5 * 1024 * 1024,
+                    // Allow to upload PNG and JPG.
+                    imageAllowedTypes: ['jpeg', 'jpg', 'png'],
+                  }}
+                  tag='textarea'
+                />
               </div>,
             )}
           </FormItem>
